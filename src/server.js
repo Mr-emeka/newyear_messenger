88c63d72b07d928c019fd4a481e55168c1275bae
mohammed <ibrahimdamy7@gmail.com>
2020-02-26 18:41:11 +0100

    ch(hosting issue06)

--- src/server.js
+++ src/server.js
@@ -32,7 +32,6 @@ app.use(cors());
 
 //ejs
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 
