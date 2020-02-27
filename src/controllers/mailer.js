f4f06714071e0762dd70cf6d6c573822d499bccd
mohammed <ibrahimdamy7@gmail.com>
2020-02-27 13:42:42 +0100

    ch(document readme file): create swagger file and comment code

--- src/controllers/mailer.js
+++ src/controllers/mailer.js
@@ -3,8 +3,12 @@ import pool from '../models/database';
 import transporter from '../helpers/mailer';
 import jsonResponse from '../helpers/response';
 
+// get date
 let selectedDay = new Date();
 
+/*
+** format date 
+*/
 let year,
     month,
     date,
@@ -19,71 +23,67 @@ hour = selectedDay.getHours();
 minute = selectedDay.getMinutes();
 second = selectedDay.getSeconds();
 
+// add 2 second to the current seconds
 let secondUse = second + 2;
 
-// console.log(secondUse)
+let dates = new Date(year, month, date, hour, minute, secondUse);
 
-var dates = new Date(year, month, date, hour, minute, secondUse);
-
-
-let theDate;
-
+// convert date toJson 2020-02-27T01-24PM
 let compare = selectedDay.toJSON();
-export let comparing = compare.substr(0, 16);
-console.log(comparing)
-var j = schedule.scheduleJob('* * * * * *', async function () {
-    try{
-        const getEntries = await pool.query(`SELECT date FROM messages`);
 
-        theDate = getEntries.rows
-        // console.log(theDate)
-    
-    
-        for (let i = 0; i < theDate.length; i++) {
-            let getData = await pool.query(`SELECT * FROM messages WHERE date=$1`, [comparing]);
-    // console.log(getData)
-    if(!getData.rows[0]){
-        return console.log('no match') 
-    }
-            else if(getData.rows[0].sent === true) {
+/*
+** get the first 16 characters
+** export comparing to middleware
+*/
+export let comparing = compare.substr(0, 16);
+
+async function sender() {
+    try {
+        // get values from the database
+        let getData = await pool.query(`SELECT * FROM messages WHERE date=$1`, [comparing]);
+
+        // if current date matchs a record in the db 
+        if (getData.rows[0]) {
+            // if message was already sent
+            if (getData.rows[0].sent === true) {
                 return console.log('message already sent')
             };
-    
-            if (theDate[i].date === comparing) {
-                // if(comparing === date.date) {
-                // console.log('here oooooooo')
-                var j = schedule.scheduleJob(dates, async function () {
-                    
-    
-                    let mailOptions = {
-                        from: 'damilolaibrahim77@gmail.com',
-                        to: `${getData.rows[0].receiver_emails[0]}`,
-                        subject: `${getData.rows[0].subject}`,
-                        html: `<p>${getData.rows[0].message}</p>
-                <div style="margin-top: 3em">
-                <p>Regards</p>
-                <p>${getData.rows[0].name}</p>
-                ${getData.rows[0].sender_email}</div>`
-                    }
-                    // send mail
-                    transporter.sendMail(mailOptions, (error, info) => {
-                        if (error) {
-                            return console.log('an error', error);
-                        }
-                        console.log('message sent', info);
-                    });
+            
+            // schedule mailer
+            let job = schedule.scheduleJob(dates, async function () {
 
-                    await pool.query(`UPDATE messages SET sent=$1 WHERE id=$2`, [true, getData.rows[0].id], );
-                    
-                    console.log('here oooooooo')
+                // email values and format
+                let mailOptions = {
+                    from: 'Seamless-mailer',
+                    to: `${getData.rows[0].receiver_emails[0]}`,
+                    subject: `${getData.rows[0].subject}`,
+                    html: `<p>${getData.rows[0].message}</p>
+                    <div style="margin-top: 3em">
+                    <p>Regards</p>
+                    <p>${getData.rows[0].name}</p>
+                    ${getData.rows[0].sender_email}</div>`
+                }
+                // send mail
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        return console.log('an error', error);
+                    }
+                    console.log('message sent', info);
                 });
-    
-            }
-    
+
+                // update sent value of a message from false to true
+                await pool.query(`UPDATE messages SET sent=$1 WHERE id=$2`, [true, getData.rows[0].id]);
+            });
+        }
+        else {
+            // if current date doesn't match a record in the db 
+            return console.log('no match');
         }
     }
     catch (e) {
         console.log(e);
     }
- 
-});
+}
+
+// set function to run in 1 second interval
+let time = setInterval(sender, 1000);
